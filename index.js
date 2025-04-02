30377411bf85e23d11c1ad50ed0099519b6f1c0c
emrd434 <emrd434@gmail.com>
2025-04-02 14:02:43 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -35,4 +35,6 @@ app.get("/dns-lookup", async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("Running on http://localhost:3000"));
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log("Running on http://localhost:3000"));
